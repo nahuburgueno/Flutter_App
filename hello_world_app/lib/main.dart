7cdb359fc582720e3a8367b94c6ecfef6cc9abc6
Nahuel Burgueño <nahuelburge@gmail.com>
2024-01-22 20:22:18 -0300

    Section 4: Flutter Primeros pasos  (Finish)

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Hola Mundo')),
-      )
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+        home: const CounterFunctionsScreen());
   }
 }
